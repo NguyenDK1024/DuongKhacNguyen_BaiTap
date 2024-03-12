10ffa87e0d264f5bc15ba6424e8e1ef6ef894f94
TUF LAPTOP <TUF LAPTOP@TUF>
2024-03-13 00:05:45 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuongKhacNguyen_BaiTap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bai 1
+            //Console.Write("nhap vao so nguyen A = ");
+            //int A = int.Parse(Console.ReadLine());
+            //if (A % 3 == 0) Console.WriteLine("So {0} chia het cho 3",A);
+            //else Console.WriteLine("So {0} khong chia het cho 3", A);
+            ///////////////////////////////////////////////
+
+            //Bai 2
+            //Console.Write("nhap vao so nguyen duong (n tu 1 den 10) n = ");
+            //int n = int.Parse(Console.ReadLine());
+            //if (n > 10 || n < 1) Console.WriteLine("So nay khong hop le");
+            //else
+            //{
+            //    for (int i = 1; i <= n; i++)
+            //    {
+            //        for (int j = 0; j <= 10; j++)
+            //        {
+            //            if (j == 0) Console.WriteLine("Bang cuu chuong {0}", i);
+            //            else Console.WriteLine("{0} x {1} = {2}", i, j, i * j);
+            //        }
+            //    }
+            //}
+            ////////////////////////////////////////////////
+
+            ////Bai 3
+            //Console.Write("nhap vao so nguyen duong (n > 0) n = ");
+            //int n = int.Parse(Console.ReadLine());
+            //int s = 0;
+            //if (n < 1) Console.WriteLine("So nay khong hop le");
+            //else
+            //{                
+            //    for (int i = n; i > 0; i--)
+            //    {
+            //        int t = 1;                   
+            //        for(int j = i; j > 0; j--)
+            //        {
+            //            t=t*j;
+            //            if(j==1) s=s+t;
+            //        }
+            //        if (i == 1) Console.WriteLine("Tong giai thua tu 1 den {0} la: {1}",n, s);
+            //    }                
+            //}
+            //////////////////////////////////////////////////////////////////////
+            Console.ReadKey();
+        }
+      
+    }
+}
